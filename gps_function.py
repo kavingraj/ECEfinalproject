0c43a846459f15f1484eef262417c029822a6cb0
rizzigalibut <128274222+rizzigalibut@users.noreply.github.com>
2023-03-19 13:11:21 -0700

    initial commit
    Takes decoded_text variable and associates with specific path. Doesn't work with just '1' and '2' so added '00' and confirmed it works.

---
+++ gps_function.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def gps(path):
+    if path == '001':
+        csvfile1 = pd.read_csv('path1.csv')
+        print(csvfile1)
+        return False
+    elif path == '002':
+        csvfile2 = pd.read_csv('path2.csv')
+        print(csvfile2)
+        return False
+    elif path == '003':
+        csvfile3 = pd.read_csv('path3.csv')
+        print(csvfile3)
+        return False
+    else:
+        print('NOTHING!!!')
+        #doNothing()
+    return True
+
+if __name__ == '__main__':
+    gps(decoded_text)
